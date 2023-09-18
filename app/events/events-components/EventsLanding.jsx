7e05d9878d29bae8a98a4eb39ce0a5cca9c8a474
Tedric <chow.tedric@gmail.com>
2023-09-18 17:36:57 +0800

    Added base DTT design

--- app/events/events-components/EventsLanding.jsx
+++ app/events/events-components/EventsLanding.jsx
@@ -1,26 +1,21 @@
 import styles from "@/app/events/events-components/EventsLanding.module.css";
-import Image from "next/image";
 
 function EventsLanding({ title, text, backgroundImage }) {
   const style = {
     backgroundImage: `linear-gradient(
       to bottom,
-      rgba(0, 0, 0, 0.6),
-      rgba(0, 0, 0, 0)
+      rgba(0, 0, 0, 0.1), 
+      rgba(0, 0, 0, 0.6),   
+      rgba(0, 0, 0, 0.1)  
     ), url(${backgroundImage})`,
   };
 
   return (
     <div className={styles.landing} style={style}>
-      {/* <Image
-        src={backgroundImage}
-        width={"100%"}
-        height={"100%"}
-        className={styles.img}
-        alt={title}
-      /> */}
-      <h1 className={styles.title}>{title}</h1>
-      <div className={styles.text}>{text}</div>
+      <div className={styles.description}>
+        <h1 className={styles.title}>{title}</h1>
+        <div className={styles.text}>{text}</div>
+      </div>
     </div>
   );
 }
--- app/events/page.jsx
+++ app/events/page.jsx
@@ -15,6 +15,94 @@ function page() {
           "https://github.com/tedricc/ntu-mj/blob/main/public/events-banner.jpg?raw=true"
         }
       />
+      <EventsLanding
+        title={"PRELUDE: OUR STORY"}
+        // text={
+        //   "ANG WEI QING (Opening) YEO REN PING (Hip Hop) JANICE GOH (Open) XENIA TAN (Open) LEONG YAN TING (Mandopop) ALEXIS LYE(Litefeet) ERNEST YEE (Litefeet) JACY HENG (Litefeet) LAU LEON (Litefeet)"
+        // }
+        backgroundImage={
+          "https://github.com/tedricc/ntu-mj/blob/main/public/alumni-dtt.jpg?raw=true"
+        }
+      />
+      <EventsLanding
+        title={"ACT 1: US | Eras"}
+        // text={"AMANDA PHUA (SIM DreamWerkz) JORDAN YU MARCUS CHING YI JINA"}
+        backgroundImage={
+          "https://github.com/tedricc/ntu-mj/blob/main/public/mjmj-dtt.jpg?raw=true"
+        }
+      />
+      <EventsLanding
+        title={"ACT 2: My Conviction | Awakening"}
+        backgroundImage={
+          "https://github.com/tedricc/ntu-mj/blob/main/public/lovy-dtt.jpg?raw=true"
+        }
+      />
+      <EventsLanding
+        title={"ACT 2: My Conviction | Our Voices"}
+        backgroundImage={
+          "https://github.com/tedricc/ntu-mj/blob/main/public/gs-dtt.jpg?raw=true"
+        }
+      />
+      <EventsLanding
+        title={"ACT 3: Our Inner World | Through the Night"}
+        backgroundImage={
+          "https://github.com/tedricc/ntu-mj/blob/main/public/ky-dtt.jpg?raw=true"
+        }
+      />
+      <EventsLanding
+        title={"ACT 3: Our Inner World | Amidst Disorientation"}
+        backgroundImage={
+          "https://github.com/tedricc/ntu-mj/blob/main/public/cyc-dtt.jpg?raw=true"
+        }
+      />
+      <EventsLanding
+        title={"ACT 3: Our Inner World | We Find Our Fight"}
+        backgroundImage={
+          "https://github.com/tedricc/ntu-mj/blob/main/public/unichlo-dtt.jpg?raw=true"
+        }
+      />
+      <EventsLanding
+        title={"ACT 3: Our Inner World | Steadfast in the Chaos"}
+        backgroundImage={
+          "https://github.com/tedricc/ntu-mj/blob/main/public/javash-dtt.jpg?raw=true"
+        }
+      />
+      <EventsLanding
+        title={"ACT 4: Our Connections | Me to We"}
+        backgroundImage={
+          "https://github.com/tedricc/ntu-mj/blob/main/public/alumni-dtt.jpg?raw=true"
+        }
+      />
+      <EventsLanding
+        title={"ACT 4: Our Connections | Unit."}
+        backgroundImage={
+          "https://github.com/tedricc/ntu-mj/blob/main/public/danheng-dtt.jpg?raw=true"
+        }
+      />
+      <EventsLanding
+        title={"ACT 5: Our Dream Play | Imagine If"}
+        backgroundImage={
+          "https://github.com/tedricc/ntu-mj/blob/main/public/din-dtt.jpg?raw=true"
+        }
+      />
+      <EventsLanding
+        title={"ACT 5: Our Dream Play | The Ambiguity of Exploration"}
+        backgroundImage={
+          "https://github.com/tedricc/ntu-mj/blob/main/public/gin-dtt.jpg?raw=true"
+        }
+      />
+      <EventsLanding
+        title={"ACT 5: Our Dream Play | Where The Flock Are We Going"}
+        backgroundImage={
+          "https://github.com/tedricc/ntu-mj/blob/main/public/bird-dtt.jpg?raw=true"
+        }
+      />
+      <EventsLanding
+        title={"THIS IS MJ"}
+        backgroundImage={
+          "https://github.com/tedricc/ntu-mj/blob/main/public/finale-dtt.jpg?raw=true"
+        }
+      />
       <Footer />
     </>
   );
