4e9f96ef0a9322cb4119ddf12f6a94fbcb5e2971
Tedric <chow.tedric@gmail.com>
2023-09-19 22:19:10 +0800

    Added home video

--- app/home-components/HomeLanding.jsx
+++ app/home-components/HomeLanding.jsx
@@ -3,7 +3,11 @@ import styles from "@/app/home-components/HomeLanding.module.css";
 function HomeLanding() {
   return (
     <div className={styles.landing}>
-      <h1 className={styles.title}>This is MJ</h1>
+      <video autoPlay loop muted className={styles.video}>
+        <source src="/home-video.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <h1 className={styles.title}>We are MJ</h1>
     </div>
   );
 }
