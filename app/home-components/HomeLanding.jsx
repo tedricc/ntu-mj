ed267de3dd226846e8139754a87cddebd390335c
Tedric <chow.tedric@gmail.com>
2023-09-20 11:02:08 +0800

    Added placeholders for home page

--- app/home-components/HomeLanding.jsx
+++ app/home-components/HomeLanding.jsx
@@ -3,7 +3,14 @@ import styles from "@/app/home-components/HomeLanding.module.css";
 function HomeLanding() {
   return (
     <div className={styles.landing}>
-      <video autoPlay loop muted className={styles.video}>
+      <video
+        autoPlay
+        loop
+        muted
+        className={styles.video}
+        playsInline
+        poster="/home-poster.jpg"
+      >
         <source src="/home-video.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
