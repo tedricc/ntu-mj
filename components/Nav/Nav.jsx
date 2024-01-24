ee8b7bf98f1002a6e86418e6c66e897fed27e86f
Tedric <chow.tedric@gmail.com>
2024-01-24 23:10:22 +0800

    Added mobile menu

--- components/Nav/Nav.jsx
+++ components/Nav/Nav.jsx
@@ -1,49 +1,107 @@
+"use client";
+
 import styles from "@/components/Nav/Nav.module.css";
 import Image from "next/image";
 import Logo from "@/public/ntu-mj-icon.png";
 import Link from "next/link";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { FaTimes } from "react-icons/fa";
+import { useState } from "react";
 
 function Nav() {
+  const [menu, setMenu] = useState(false);
+
+  function handleMenu() {
+    if (!menu) {
+      setMenu(true);
+      console.log("open menu");
+    } else {
+      setMenu(false);
+      console.log("close menu");
+    }
+  }
+
   return (
-    <div className="container">
-      <div className="row">
-        <div className={styles.nav}>
-          <Link href="/" className={styles.home}>
-            <figure className={styles.logo__wrapper}>
-              <Image
-                src={Logo}
-                width={48}
-                height={48}
-                alt="MJ Logo"
-                className={styles.logo}
-                priority
-              />
-            </figure>
-            <div className={styles.title}>NTU MJ</div>
-          </Link>
+    <>
+      {menu && (
+        <>
+          <div className={styles.menu}>
+            <div className={styles.menu__close}>
+              <FaTimes onClick={handleMenu} />
+            </div>
 
-          <div className={styles.link__wrapper}>
-            <Link href="/" className={styles.link}>
-              Home
-            </Link>
-
-            <Link href="/about" className={styles.link}>
-              About
-            </Link>
-
-            <Link href="/events" className={styles.link}>
-              Events
-            </Link>
-
-            <Link href="/alumni" className={styles.link}>
-              Alumni
-            </Link>
+            <div className={styles.menu__links}>
+              <Link href="/" className={styles.menu__link} onClick={handleMenu}>
+                Home
+              </Link>
+              <Link
+                href="/about"
+                className={styles.menu__link}
+                onClick={handleMenu}
+              >
+                About
+              </Link>
+              <Link
+                href="/events"
+                className={styles.menu__link}
+                onClick={handleMenu}
+              >
+                Events
+              </Link>
+              <Link
+                href="/alumni"
+                className={styles.menu__link}
+                onClick={handleMenu}
+              >
+                Alumni
+              </Link>
+            </div>
           </div>
+        </>
+      )}
+      
+      <div className="container">
+        <div className="row">
+          <div className={styles.nav}>
+            <Link href="/" className={styles.home}>
+              <figure className={styles.logo__wrapper}>
+                <Image
+                  src={Logo}
+                  width={48}
+                  height={48}
+                  alt="MJ Logo"
+                  className={styles.logo}
+                  priority
+                />
+              </figure>
+              <div className={styles.title}>NTU MJ</div>
+            </Link>
 
-          {/* add ur burger here + other stuff */}
+            <div className={styles.link__wrapper}>
+              <Link href="/" className={styles.link}>
+                Home
+              </Link>
+
+              <Link href="/about" className={styles.link}>
+                About
+              </Link>
+
+              <Link href="/events" className={styles.link}>
+                Events
+              </Link>
+
+              <Link href="/alumni" className={styles.link}>
+                Alumni
+              </Link>
+
+              <div className={styles.menu__btn}>
+                <GiHamburgerMenu onClick={handleMenu} />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
